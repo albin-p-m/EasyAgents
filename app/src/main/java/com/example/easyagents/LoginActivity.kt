0b569577b4e27eab51624a6d4303e12776743478
Albin <albinalbuss@gmail.com>
2021-06-10 05:45:00 +0530

    navigation from login page to register page and also from register page to login page implemented

--- app/src/main/java/com/example/easyagents/LoginActivity.kt
+++ app/src/main/java/com/example/easyagents/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.easyagents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val tvRegister: TextView = findViewById(R.id.tvRegisterLogin)
+
+        tvRegister.setOnClickListener{
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+            onStop()
+        }
     }
 }
--- app/src/main/java/com/example/easyagents/RegisterActivity.kt
+++ app/src/main/java/com/example/easyagents/RegisterActivity.kt
@@ -1,11 +1,22 @@
 package com.example.easyagents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val tvLogin: TextView = findViewById(R.id.tvLoginRegister)
+
+        tvLogin.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            onStop()
+        }
+        
     }
 }
